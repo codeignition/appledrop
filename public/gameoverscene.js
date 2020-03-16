532cfe8b86ffb8606368bd965faeea5917c9e4a2
Deven Bhooshan <deven.b@go-jek.com>
2020-03-16 23:13:01 +0530

    add play again button in game over scene

--- public/gameoverscene.js
+++ public/gameoverscene.js
@@ -17,6 +17,20 @@ class GameOverScene extends Phaser.Scene{
 		});
 		title.setPosition(game.config.width/2 - (game.config.width / 20 + 100), game.config.height/2);
 
+		const playButton = this.add.text(game.config.width/2 - (game.config.width / 20  + 100), game.config.height/2 + 50, 'PLAY AGAIN', { fontSize: game.config.width / 20});
+	    playButton.setInteractive();
+	    
+	    var self = this;
+	    playButton.on('pointerdown', () => { 
+	    	console.log(new Date().getTime() - self.previousTime > 10000);
+	    	if(self.previousTime == null || new Date().getTime() - self.previousTime > 1000) {
+		
+		    	this.scene.start('PlayGame');
+		    	self.previousTime = new Date().getTime();
+	        }
+
+	    });
+
 	}
 
 }
