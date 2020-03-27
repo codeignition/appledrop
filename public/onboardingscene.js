508546f2880a08d69e3751ddda57daa61b07aca2
Deven Bhooshan <deven.b@go-jek.com>
2020-03-27 23:22:26 +0530

    update onboarding image

--- public/onboardingscene.js
+++ public/onboardingscene.js
@@ -6,16 +6,24 @@ class OnboardingScene extends Phaser.Scene {
 
     preload() {
         this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#697e96");
-        this.load.image("onboarding", "assets/onbaording-2-small.png");
+        this.load.image("onboarding", "assets/onboarding-4.png");
     }
 
     create() {
+        this.bg = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bg-menu')
+        let scaleX = this.cameras.main.width / this.bg.width
+        let scaleY = this.cameras.main.height / this.bg.height
+        let scale = Math.max(scaleX, scaleY)
+
+        this.bg.setScale(scale).setScrollFactor(0);
+        this.bg.tint = Phaser.Display.Color.GetColor32(59, 59, 59, .1);
+
 
         var playerSprite = this.physics.add.sprite(0, 0, 'onboarding');
         playerSprite.setOrigin(0, 0);
 
 
-        const playButton = this.add.text(game.config.width / 2 - (game.config.width / 20 + 100), game.config.height / 2 + 100, 'TAP HERE TO PLAY', {fontSize: game.config.width / 20});
+        const playButton = this.add.text(game.config.width / 2 - (game.config.width / 20 + 80), game.config.height / 2 + 200, 'TAP HERE TO PLAY', {fontSize: game.config.width / 20});
         playButton.setInteractive();
 
         var self = this;
